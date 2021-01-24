ccbccf81a3b7bf57b4069f3bcea89ec62b000cec
Uman Shahzad <uman@mslm.io>
2021-01-24 07:45:00 +0500

    placeholder files to get started.

---
+++ scripts/fmt.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DIR=`dirname $0`
+ROOT=$DIR/..
+
+# Format code in project.
+
+find \
+    $ROOT/src \
+    $ROOT/index.js \
+    $ROOT/package.json \
+    -not \( -path $ROOT/node_modules -prune \) \
+    -name '*.js' -print0 \
+    -or -name '*.ts' -print0 \
+    -or -name '*.css' -print0 \
+    -or -name '*.scss' -print0 \
+    -or -name '*.html' -print0 \
+    -or -name '*.json' -print0 \
+    | xargs -0 $ROOT/node_modules/.bin/prettier \
+        --config $ROOT/.prettierrc.js \
+        --write
