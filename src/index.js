db30f95089e20fe5e651ed1e10bbd13ff35ea7cf
Soban Mahmod <soban.mahmood@mslm.io>
2022-07-19 21:53:51 +0500

    updated index.js directory and used default ip selector

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const { IPinfoWrapper } = require("node-ipinfo");
+const defaultIPSelector = require("./ip-selector/default-ip-selector");
+
+module.exports = ({ token = "", cache, timeout, ipSelector }) => {
+    const ipinfo = new IPinfoWrapper(token, cache, timeout);
+    if (ipSelector == null || typeof(ipSelector) != 'function') {
+        ipSelector = defaultIPSelector;
+    }
+    return async (req, _, next) => {
+        const ip = ipSelector(req);
+        req.ipinfo = await ipinfo.lookupIp(ip);
+        next();
+    };
+};
+
+module.exports.defaultIPSelector = defaultIPSelector;
